4d39d9de292f20f56848566e23cc7be5312e1cd0
Mira Yang <55816593+miramirrormira@users.noreply.github.com>
2022-05-14 22:40:17 -0400

    Delete tutorials directory

--- tutorials/ModelsInR/hierarchicalClustering.R
+++
@@ -1,8 +0,0 @@
-########################################
-### Hierarchical Clustering Example
-### Discription: use hierarchical clustering model to classify US States based on their criminal rate
-########################################
-
-hc <- hclust(dist(USArrests), "ave")
-plot(hc)
-plot(hc, hang = -1)
